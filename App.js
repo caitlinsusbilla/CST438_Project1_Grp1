51e90efa79f0e1b18d40be2c3c49a1e61cc2618d
Enrique <enrangel@csumb.edu>
2024-09-08 13:11:52 -0700

    adding screen files

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image , ImageBackground, ScrollView, Button, Pressable} from "react-native";
+const logoImg = require("./assets/pokeball-png-45334.png")
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    // this will show the same of the app in a pikachu colorway and will add the background
+  <View style={{flex: 1, backgroundColor: "#F0EAD6", padding: 100 }}>
+
+
+    {/* <Text>
+      <Text style={{ color: "#e92929" }}>Po</Text>
+      <Text style={{ color: "#f4dc26" }}>keA</Text>
+      <Text style={{ color: "#e92929" }}>pp</Text>
+    </Text> */}
+
+
+    <Button title="Login" onPress={() => console.log("Welcome")} color="#e92929"/>
+
+    
+    <ImageBackground source={logoImg} style={{flex: 20 }}></ImageBackground>
+    {/* <Image source={logoImg} style={{ width: 200, height: 200 }}/> */}
+
+  </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
