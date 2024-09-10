d4018d1b1e6693810367c980a7e0666b07cbdb34
Sabino Galindo <galindo.sabino831@gmail.com>
2024-09-10 10:44:24 -0700

    Created a SQLite databse that gets the first 150 pokemon from the PokeAPI. Also created a simple Pokedex screen that makes sure the API is being saved correctly

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./screens/HomeScreen";
 import Pokedex from './screens/Pokedex';
 import Profile from './screens/Profile';
-
+import { initDatabase } from './utils/database';
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
+  useEffect(() => {
+    initDatabase()
+      .then(() => console.log('Database initialized successfully'))
+      .catch(error => console.error('Failed to initialize database:', error));
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='HomeScreen'>
