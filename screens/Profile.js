10446536d6b5c376d5b29c6b74247ef255ababde
aharo1110 <aharo1110@gmail.com>
2024-09-12 10:37:29 -0700

    Getting started

--- screens/Profile.js
+++ screens/Profile.js
@@ -4,6 +4,10 @@ import { View, Text } from 'react-native';
 export default function Profile() {
     return (
         <View>
+            {/* Specifications
+            -- Name
+            -- Party
+            -- Bio */}
             <Text>Profile</Text>
         </View>
     );
